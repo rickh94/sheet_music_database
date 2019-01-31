89c69f291650ae6023e9daa0c3c6e4438e88ca47
Rick Henry <fredericmhenry@gmail.com>
2019-01-30 22:21:39 -0500

    improve layout, add font awesome
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- frontend/components/App/App.jsx
+++ frontend/components/App/App.jsx
@@ -3,12 +3,17 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import {library } from '@fortawesome/fontawesome-svg-core'
+import { faUser, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 import 'normalize.css'
 
 import stuff from '../../reducers'
 import Home from '../Home/Home'
 
 let store = createStore(stuff, applyMiddleware(thunk))
+
+library.add(faUser)
+library.add(faUserPlus)
 
 export default class App extends Component {
   async componentDidMount() {}
--- frontend/components/Home/Home.jsx
+++ frontend/components/Home/Home.jsx
@@ -1,26 +1,27 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 import Card from 'react-bulma-components/lib/components/card'
 import Container from 'react-bulma-components/lib/components/container'
 import Heading from 'react-bulma-components/lib/components/heading'
 import Content from 'react-bulma-components/lib/components/content'
-import Box from 'react-bulma-components/lib/components/box'
-import Section from 'react-bulma-components/lib/components/section'
+import Columns from 'react-bulma-components/lib/components/columns'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-import backgroundImage from './background.jpg'
+// import backgroundImage from './background.jpg'
 import './Home.scss'
 import Header from '../Header/Header'
 
-const background = {
-  position: 'absolute',
-  top: '0px',
-  left: '0px',
-  width: '100vw',
-  height: '100vh',
-  filter: 'blur(1px) opacity(5%) grayscale(6)',
-  zIndex: 1
-}
+// const background = {
+//   position: 'fixed',
+//   top: '0px',
+//   left: '0px',
+//   width: '100vw',
+//   height: '100vh',
+//   filter: 'blur(1px) opacity(5%) grayscale(6)',
+//   zIndex: 1
+// }
 
 export default class Home extends Component {
   render() {
@@ -28,7 +29,7 @@ export default class Home extends Component {
       <React.Fragment>
         <Header />
         <Container className="foreground">
-          <Card color="white" className="margin-default mostly-opaque">
+          <Card className="margin-default mostly-opaque">
             <Card.Content style={{ paddingTop: '1vh' }}>
               <Heading size={3}>Sheet Music Database</Heading>
               <Heading subtitle={true}>Organize your sheet music</Heading>
@@ -37,12 +38,12 @@ export default class Home extends Component {
             <Card.Footer>
               <Card.Footer.Item>
                 <Link to="/login" className="card--link">
-                  Login
+                  <FontAwesomeIcon icon="user" /> Login
                 </Link>
               </Card.Footer.Item>
               <Card.Footer.Item>
                 <Link to="/register" className="card--link">
-                  Register
+                  <FontAwesomeIcon icon="user-plus" /> Register
                 </Link>
               </Card.Footer.Item>
             </Card.Footer>
@@ -51,62 +52,62 @@ export default class Home extends Component {
           <Heading size={4} className="margin-default">
             Organization
           </Heading>
-          <Card className="margin-default description-card">
-            <Card.Header>
-              <Card.Header.Title>Piece</Card.Header.Title>
-            </Card.Header>
-            <Card.Content>
-              <Content>
-                Everything is organized into pieces which have:
-                <ul>
-                  <li>Composer(s): The composer of the piece</li>
-                  <li>Sheets: The individual sheet music files that correspond to the piece</li>
-                  <li>Tags: Arbitrary tags for categorizing pieces</li>
-                  <li>Instruments: All the instruments connected with a piece{"'"}s sheets</li>
-                </ul>
-                This provides one place for all the data about a piece can be found, while still having separate data
-                that can be filtered in other ways
-              </Content>
-            </Card.Content>
-          </Card>
-          <Card className="margin-default description-card">
-            <Card.Header>
-              <Card.Header.Title>Composer</Card.Header.Title>
-            </Card.Header>
-            <Card.Content>
-              <Content>
+          <DescriptionCard title="Piece">
+            Everything is organized into pieces which have:
+            <ul>
+              <li>Composer(s): The composer of the piece</li>
+              <li>Sheets: The individual sheet music files that correspond to the piece</li>
+              <li>Tags: Arbitrary tags for categorizing pieces</li>
+              <li>Instruments: All the instruments connected with a piece{"'"}s sheets</li>
+            </ul>
+            This provides one place for all the data about a piece can be found, while still having separate data that
+            can be filtered in other ways
+          </DescriptionCard>
+          <Columns className="margin-default">
+            <Columns.Column>
+              <DescriptionCard title="Composer" column>
                 Composers are separate objects from Pieces so that multiple pieces can share the same composer and you
                 can easily see all pieces associated with a composer. It holds all the data about a composer, such as
                 their name, birth/death dates, and the era/style of their compositions.
-              </Content>
-            </Card.Content>
-          </Card>
-          <Card className="margin-default description-card">
-            <Card.Header>
-              <Card.Header.Title>Sheet</Card.Header.Title>
-            </Card.Header>
-            <Card.Content>
-              <Content>
+              </DescriptionCard>
+            </Columns.Column>
+            <Columns.Column>
+              <DescriptionCard title="Sheet" column>
                 A Sheet is actual sheet music with an associated file. It holds all the data about the file, such as the
                 instrument(s) in the sheet, the file format, and what type of sheet it is (e.g. score, part,
                 manuscript), as well as being able to get the file itself.
-              </Content>
-            </Card.Content>
-          </Card>
-          <Card className="margin-default description-card">
-            <Card.Header>
-              <Card.Header.Title>Tag</Card.Header.Title>
-            </Card.Header>
-            <Card.Content>
-              <Content>
+              </DescriptionCard>
+            </Columns.Column>
+            <Columns.Column>
+              <DescriptionCard title="Tag" column>
                 A Tag (like in many apps) is any arbitrary text to be associated with a piece to descibe its attributes
                 and associate it with other pieces with those attributes.
-              </Content>
-            </Card.Content>
-          </Card>
+              </DescriptionCard>
+            </Columns.Column>
+          </Columns>
         </Container>
-        <img style={background} src={backgroundImage} />
+        {/* <img style={background} src={backgroundImage} /> */}
       </React.Fragment>
     )
   }
+}
+
+function DescriptionCard(props) {
+  const marginClass = props.column ? '' : 'margin-default'
+  return (
+    <Card className={`${marginClass} description-card`}>
+      <Card.Header>
+        <Card.Header.Title>{props.title}</Card.Header.Title>
+      </Card.Header>
+      <Card.Content>
+        <Content>{props.children}</Content>
+      </Card.Content>
+    </Card>
+  )
+}
+
+DescriptionCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.any.isRequired,
+  column: PropTypes.bool
 }
