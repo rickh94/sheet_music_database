debc9443dd923e5f1ff30ae3aaf7900f9315c6c8
Rick Henry <fredericmhenry@gmail.com>
2019-04-11 02:20:35 -0400

    add instruments to actions index
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- frontend/actions/index.js
+++ frontend/actions/index.js
@@ -1,5 +1,6 @@
 import * as account from './account'
 import * as tags from './tags'
 import * as composers from './composers'
+import * as instruments from './instruments'
 
-export { account, tags, composers }
+export { account, tags, composers, instruments }
